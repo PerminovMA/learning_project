d3b2c7b9e82ff731c86a42a233944140a04bfae2
PerminovMA <Perminov777@mail.ru>
2015-02-22 22:00:05 +0300

    templates added

--- learning_app/views.py
+++ learning_app/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello world!")
-
+    context = {}
+    return render(request, 'site/index.html', context)
 
 def get_data(request):
     return HttpResponse("Get data")
